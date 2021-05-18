97e41fcb1b44f58c5bbaca7aef09933f3f3c91ed
anjali <anjalikrshna609@gmail.com>
2021-05-18 13:59:25 +0530

    Database store Services,dashboard creation

--- Angular/BankApp/src/app/login/login.component.ts
+++ Angular/BankApp/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +13,9 @@ export class LoginComponent implements OnInit {
   accno="Account No please"
   pswd="";
   usname="";
-  accountDetails:any={
-    1000:{acno:1000,actype:"savings",username:"userone",password:"userone",balance:50000},
-    1001:{acno:1001,actype:"savings",username:"usertwo",password:"usertwo",balance:5000},
-    1002:{acno:1002,actype:"current",username:"userthree",password:"userthree",balance:10000},
-    1003:{acno:1003,actype:"current",username:"userfour",password:"userfour",balance:6000}
-        } 
+  
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private dataservice:DataService) { }
 
   ngOnInit(): void {
   }
@@ -31,20 +27,16 @@ export class LoginComponent implements OnInit {
     var acno=this.accno;
     var pwd=this.pswd;
     var uname=this.usname;
-    let users=this.accountDetails;
-            if(acno in users)
-            {
-            if(uname==users[acno]["username"]&&pwd==users[acno]["password"])
-            this.router.navigateByUrl("dashboard");
-            else
-            alert("Invalid username or password")
-            }
-            else
-            alert("Invalid Account No")
-            
+    const res=this.dataservice.login(acno,pwd,uname);
+    if(res)
+    {alert("Login Successful")
+      this.router.navigateByUrl("dashboard");}
   }
+
   register()
   {
     this.router.navigateByUrl("register");
   }
 }
+
+
--- Angular/BankApp/src/app/register/register.component.ts
+++ Angular/BankApp/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  accno="";
+  pswd="";
+  uname="";
 
-  constructor() { }
+  constructor(private dataservice:DataService,private router:Router) { }
 
   ngOnInit(): void {
   }
 
+reg()
+{
+  var acno=this.accno;
+  var pswd=this.pswd;
+  var uname=this.uname;
+  const result=this.dataservice.reg(acno,pswd,uname);
+  if(result)
+  {
+    alert("Successfully registered");
+    this.router.navigateByUrl("");
+  }
+  else{
+    alert("User already Exists");
+    alert("Please Login");
+  }
+  }
 }
+
+
+
